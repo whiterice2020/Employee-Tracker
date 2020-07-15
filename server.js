092cda8bf24ac7c467f8b81696d111fb0ccd3079
Paul DelPadre <PDelPadre@rice.local>
2020-07-15 16:42:47 -0400

    creating functions and questions

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var mysql = require("mysql");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -10,3 +13,154 @@ var connection = mysql.createConnection({
     password: "root",
     database: "employee_tracker"
 });
+
+// connect to the mysql server and sql database
+connection.connect(function (err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    start();
+});
+
+// Questions to ask the user
+//   This function starts when the server loads
+function start() {
+    inquirer
+        .prompt({
+            name: "whatToDo",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                "View All Employees",
+                "View All Employees by Department",
+                "View All Employees by Manager",
+                "Add Employee",
+                "Update Employee Role",
+                "Update Employee Manager",
+                "View All Roles",
+
+            ]
+        })
+        .then(function (answer) {
+            // based on their answer, either call the bid or the post functions
+            if (answer.postOrBid === "POST") {
+                postAuction();
+            }
+            else if (answer.postOrBid === "BID") {
+                bidAuction();
+            } else {
+                connection.end();
+            }
+        });
+}
+
+function addEmployee() {
+    // prompt for info about the item being put up for auction
+    inquirer
+        .prompt([
+            {
+                name: "employeeFirstName",
+                type: "input",
+                message: "What is the employee's first name?",
+
+            }
+            {
+                name: "employeeLastName",
+                type: "input",
+                message: "What is the employee's last name?",
+
+            }
+            {
+                name: "employeeRole",
+                type: "list",
+                message: "What is the employee's role?",
+                choices: [
+                    "Sales Lead",
+                    "Salesperson",
+                    "Lead Engineer",
+                    "Account Manager",
+                    "Accountant",
+                    "Legal Team Lead",
+                ]
+            }
+        ]).then(function (answer) {
+            // when finished prompting, insert a new item into the db with that info
+            connection.query(
+                "INSERT INTO auctions SET ?",
+                {
+                    item_name: answer.item,
+                    category: answer.category,
+                    starting_bid: answer.startingBid || 0,
+                    highest_bid: answer.startingBid || 0
+                },
+                function (err) {
+                    if (err) throw err;
+                    console.log("Your auction was created successfully!");
+                    // re-prompt the user for if they want to bid or post
+                    start();
+                }
+            );
+        });
+  }
+
+function bidAuction() {
+    // query the database for all items being auctioned
+    connection.query("SELECT * FROM auctions", function (err, results) {
+        if (err) throw err;
+        // once you have the items, prompt the user for which they'd like to bid on
+        inquirer
+            .prompt([
+                {
+                    name: "choice",
+                    type: "rawlist",
+                    choices: function () {
+                        var choiceArray = [];
+                        for (var i = 0; i < results.length; i++) {
+                            choiceArray.push(results[i].item_name);
+                        }
+                        return choiceArray;
+                    },
+                    message: "What auction would you like to place a bid in?"
+                },
+                {
+                    name: "bid",
+                    type: "input",
+                    message: "How much would you like to bid?"
+                }
+            ])
+            .then(function (answer) {
+                // get the information of the chosen item
+                var chosenItem;
+                for (var i = 0; i < results.length; i++) {
+                    if (results[i].item_name === answer.choice) {
+                        chosenItem = results[i];
+                    }
+                }
+
+                // determine if bid was high enough
+                if (chosenItem.highest_bid < parseInt(answer.bid)) {
+                    // bid was high enough, so update db, let the user know, and start over
+                    connection.query(
+                        "UPDATE auctions SET ? WHERE ?",
+                        [
+                            {
+                                highest_bid: answer.bid
+                            },
+                            {
+                                id: chosenItem.id
+                            }
+                        ],
+                        function (error) {
+                            if (error) throw err;
+                            console.log("Bid placed successfully!");
+                            start();
+                        }
+                    );
+                }
+                else {
+                    // bid wasn't high enough, so apologize and start over
+                    console.log("Your bid was too low. Try again...");
+                    start();
+                }
+            });
+    });
+}
