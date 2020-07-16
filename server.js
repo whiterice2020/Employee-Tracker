cf8d26c2e642961df58d4c25cfd963befb0374a4
Paul DelPadre <PDelPadre@rice.local>
2020-07-15 22:05:34 -0400

    updating questions

--- server.js
+++ server.js
@@ -102,65 +102,236 @@ function addEmployee() {
         });
   }
 
-function removeEmployee() {
-    // query the database for all items being auctioned
-    connection.query("SELECT * FROM auctions", function (err, results) {
-        if (err) throw err;
-        // once you have the items, prompt the user for which they'd like to bid on
-        inquirer
-            .prompt([
-                {
-                    name: "choice",
-                    type: "rawlist",
-                    choices: function () {
-                        var choiceArray = [];
-                        for (var i = 0; i < results.length; i++) {
-                            choiceArray.push(results[i].item_name);
-                        }
-                        return choiceArray;
-                    },
-                    message: "What auction would you like to place a bid in?"
-                },
-                {
-                    name: "bid",
-                    type: "input",
-                    message: "How much would you like to bid?"
-                }
-            ])
-            .then(function (answer) {
-                // get the information of the chosen item
-                var chosenItem;
-                for (var i = 0; i < results.length; i++) {
-                    if (results[i].item_name === answer.choice) {
-                        chosenItem = results[i];
-                    }
-                }
+  function removeEmployee() {
+    // prompt for info about the item being put up for auction
+    inquirer
+    .prompt([
+        {
+            name: "employeeFirstName",
+            type: "input",
+            message: "What is the employee's first name?",
 
-                // determine if bid was high enough
-                if (chosenItem.highest_bid < parseInt(answer.bid)) {
-                    // bid was high enough, so update db, let the user know, and start over
-                    connection.query(
-                        "UPDATE auctions SET ? WHERE ?",
-                        [
-                            {
-                                highest_bid: answer.bid
-                            },
-                            {
-                                id: chosenItem.id
-                            }
-                        ],
-                        function (error) {
-                            if (error) throw err;
-                            console.log("Bid placed successfully!");
-                            start();
-                        }
-                    );
-                }
-                else {
-                    // bid wasn't high enough, so apologize and start over
-                    console.log("Your bid was too low. Try again...");
+        }
+        {
+            name: "employeeLastName",
+            type: "input",
+            message: "What is the employee's last name?",
+
+        }
+        {
+            name: "employeeRole",
+            type: "list",
+            message: "What is the employee's role?",
+            choices: [
+                "Sales Lead",
+                "Salesperson",
+                "Lead Engineer",
+                "Account Manager",
+                "Accountant",
+                "Legal Team Lead",
+            ]
+        }
+    ]).then(function (answer) {
+        // when finished prompting, insert a new item into the db with that info
+        connection.query(
+            "INSERT INTO auctions SET ?",
+            {
+                item_name: answer.item,
+                category: answer.category,
+                starting_bid: answer.startingBid || 0,
+                highest_bid: answer.startingBid || 0
+            },
+            function (err) {
+                if (err) throw err;
+                console.log("Your auction was created successfully!");
+                // re-prompt the user for if they want to bid or post
+                start();
+            }
+        );
+    });
+  }
+
+  function updateEmployeeRole() {
+    // prompt for info about the item being put up for auction
+    inquirer
+        .prompt([
+            {
+                name: "employeeRoleUpdate",
+                type: "list",
+                message: "What is the employee's role?",
+                choices: [
+                    "Sales Lead",
+                    "Salesperson",
+                    "Lead Engineer",
+                    "Account Manager",
+                    "Accountant",
+                    "Legal Team Lead",
+                ]
+            }
+        ]).then(function (answer) {
+            // when finished prompting, insert a new item into the db with that info
+            connection.query(
+                "INSERT INTO auctions SET ?",
+                {
+                    item_name: answer.item,
+                    category: answer.category,
+                    starting_bid: answer.startingBid || 0,
+                    highest_bid: answer.startingBid || 0
+                },
+                function (err) {
+                    if (err) throw err;
+                    console.log("Your auction was created successfully!");
+                    // re-prompt the user for if they want to bid or post
                     start();
                 }
-            });
-    });
-}
+            );
+        });
+  }
+  
+  function updateEmployeeManager() {
+    // prompt for info about the item being put up for auction
+    inquirer
+        .prompt([
+            {
+                name: "updateEmployeeManager",
+                type: "list",
+                message: "What manager do you want to assign ",
+                choices: [
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                ]
+            }
+        ]).then(function (answer) {
+            // when finished prompting, insert a new item into the db with that info
+            connection.query(
+                "INSERT INTO auctions SET ?",
+                {
+                    item_name: answer.item,
+                    category: answer.category,
+                    starting_bid: answer.startingBid || 0,
+                    highest_bid: answer.startingBid || 0
+                },
+                function (err) {
+                    if (err) throw err;
+                    console.log("Your auction was created successfully!");
+                    // re-prompt the user for if they want to bid or post
+                    start();
+                }
+            );
+        });
+  }
+  function viewAllRoles() {
+    // prompt for info about the item being put up for auction
+    inquirer
+        .prompt([
+            {
+                name: "viewAllRoles",
+                type: "list",
+                message: "What manager do you want to assign ",
+                choices: [
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                ]
+            }
+        ]).then(function (answer) {
+            // when finished prompting, insert a new item into the db with that info
+            connection.query(
+                "INSERT INTO auctions SET ?",
+                {
+                    item_name: answer.item,
+                    category: answer.category,
+                    starting_bid: answer.startingBid || 0,
+                    highest_bid: answer.startingBid || 0
+                },
+                function (err) {
+                    if (err) throw err;
+                    console.log("Your auction was created successfully!");
+                    // re-prompt the user for if they want to bid or post
+                    start();
+                }
+            );
+        });
+  }
+
+  function addRole() {
+    // prompt for info about the item being put up for auction
+    inquirer
+        .prompt([
+            {
+                name: "addRole",
+                type: "list",
+                message: "What role would you like to add?",
+                choices: [
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                ]
+            }
+        ]).then(function (answer) {
+            // when finished prompting, insert a new item into the db with that info
+            connection.query(
+                "INSERT INTO auctions SET ?",
+                {
+                    item_name: answer.item,
+                    category: answer.category,
+                    starting_bid: answer.startingBid || 0,
+                    highest_bid: answer.startingBid || 0
+                },
+                function (err) {
+                    if (err) throw err;
+                    console.log("Your auction was created successfully!");
+                    // re-prompt the user for if they want to bid or post
+                    start();
+                }
+            );
+        });
+  }
+
+  function removeRole() {
+    // prompt for info about the item being put up for auction
+    inquirer
+        .prompt([
+            {
+                name: "removeRoll",
+                type: "list",
+                message: "What role would you like to remove?",
+                choices: [
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                ]
+            }
+        ]).then(function (answer) {
+            // when finished prompting, insert a new item into the db with that info
+            connection.query(
+                "INSERT INTO auctions SET ?",
+                {
+                    item_name: answer.item,
+                    category: answer.category,
+                    starting_bid: answer.startingBid || 0,
+                    highest_bid: answer.startingBid || 0
+                },
+                function (err) {
+                    if (err) throw err;
+                    console.log("Your auction was created successfully!");
+                    // re-prompt the user for if they want to bid or post
+                    start();
+                }
+            );
+        });
+  }
+
